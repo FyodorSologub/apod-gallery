f1be20de4a9d1448e83cf00117b7b68e964ae45d
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-04 03:52:34 +0300

    small fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ export const Card : React.FC<CardProps> = ({ explanation, title, url, reference 
     const explanationShort = explanation.slice(0, 100) + '...';
 
     return (
-        <article ref={reference} className="border border-slate-900 shadow-lg bg-white rounded-md w-full h-24 px-5 py-3 flex gap-x-3 relative group items-center transition-all">
+        <article ref={reference} className="border border-slate-900 shadow-lg bg-white rounded-md w-full h-24 px-5 py-3 flex gap-x-3 relative group items-center transition-all snap-always snap-center">
             <img className="w-16 h-16 aspect-square rounded-full group-hover:blur-[1px]" src={ url } alt="?" />
             <div className="flex flex-col w-full gap-0 overflow-hidden">
                 <h2 className="w-full max-h-10 rounded-sm text-lg antialiased font-thin truncate shrink-0 group-hover:blur-[1px]">{ title }</h2>
--- src/features/CardsList.tsx
+++ src/features/CardsList.tsx
@@ -4,8 +4,8 @@ import { Card, CardLoading } from "../components";
 export const CardsList : React.FC<cardsList> = ({ data, reference, isLoading }) : JSX.Element => {
 
   return (
-    <main className='h-auto p-5 flex justify-center bg-slate-100'>
-        <section className='h-full max-h-full w-4/5 max-w-[1000px] grid grid-cols-1 lg:grid-cols-2 grid-rows-auto gap-5 pt-3 lg:pt-5'>
+    <main className='h-auto mt-20 p-5 flex justify-center bg-slate-100'>
+        <section className='h-full snap-y snap-mandatory max-h-full w-4/5 max-w-[1000px] grid grid-cols-1 lg:grid-cols-2 grid-rows-auto gap-5 pt-3 lg:pt-5'>
         { data.map((obj, id) => {
             if(data.length === id + 1) {
               return <Card reference={reference} explanation={obj.explanation} title={obj.title} url={obj.url} key={id} />
