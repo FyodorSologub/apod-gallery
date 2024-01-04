b040adfb53286cdf81e6af49da1b174ab5319f56
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-04 18:48:15 +0300

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { router } from './Router';
+import { RouterProvider } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
