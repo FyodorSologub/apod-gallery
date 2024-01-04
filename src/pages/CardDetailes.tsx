201e7d9abcc71394f522fd695fb1866caead66e8
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-05 02:35:40 +0300

    small fixes

--- src/pages/CardDetailes.tsx
+++ src/pages/CardDetailes.tsx
@@ -1,5 +1,12 @@
+import { useSearchParams } from 'react-router-dom';
+
 export const CardDetailes = () => {
+  const [ searchParams ] = useSearchParams();
+
   return (
-    <h1>CardDetailes</h1>
+    <>
+      <h1>{ searchParams.get('title') }</h1>
+      <p>{ searchParams.get('explanation') }</p>
+    </>
   )
 };
