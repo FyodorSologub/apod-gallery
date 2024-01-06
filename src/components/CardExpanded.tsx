1f25a5d14c7fee7e9d23eeef09dae16c4dd5d903
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-07 02:34:50 +0300

    finished new card expanded view

--- src/components/CardExpanded.tsx
+++ src/components/CardExpanded.tsx
@@ -1,11 +1,14 @@
 import { cardExpandedProps } from '../types/index';
 import Avatar from '../assets/avatar.svg';
+import { useState } from 'react';
 
 export const CardExpanded : React.FC<cardExpandedProps>= ({ url, title, explanation, copyright }) => {
+  const [ isExpanded, setExpanded ] = useState<boolean>(false);
+
   return (  
-    <article className='w-full h-full border rounded-md border-slate-900 shadow-lg bg-slate-100 p-3 md:p-5 grid grid-cols-1 md:grid-cols-2 grid-rows-12'>
-      <h2 className='w-auto h-auto text-center rounded-md text-md lg:text-xl antialiased font-extralight shrink-0 col-span-1 md:col-span-2 row-start-1 row-end-2'>{ title }</h2>
-      <div className='border rounded-md border-slate-200 col-span-1 row-start-2 row-end-7 md:row-start-2 md:row-end-13 relative'>
+    <article className='w-full h-full border-2 rounded-md border-slate-900 shadow-lg bg-slate-100 p-3 md:p-5 grid grid-cols-1 md:grid-cols-2 grid-rows-12 relative'>
+      <h2 className='w-auto h-auto text-center rounded-md text-md antialiased font-extralight shrink-0 col-span-1 md:col-span-2 row-start-1 row-end-2'>{ title }</h2>
+      <div className={`border ${ isExpanded ? 'hidden' : 'block' } rounded-md border-slate-200 col-span-1 row-start-2 row-end-7 md:row-start-2 md:row-end-13 relative`}>
         <img className='h-full w-full aspect-square rounded-md object-cover' src={ url } />
         { copyright &&
           <div className='w-fit h-auto flex gap-x-1 md:gap-x-2 items-center justify-center shrink-0 absolute border bg-slate-950 px-2 lg:px-3 py-1 lg:py-2 rounded-md top-2 right-2'>
@@ -14,7 +17,12 @@ export const CardExpanded : React.FC<cardExpandedProps>= ({ url, title, explanat
           </div>
         }
       </div>
-      <small className='w-auto h-auto flex items-center overflow-hidden max-h-full text-xs lg:text-base antialiased font-extralight text-justify col-span-1 md:col-span-2 row-start-7 row-end-13 md:row-start-2 md:row-end-13 md:px-5'>{ explanation }</small>
+      <small className={`w-auto h-auto md:flex md:items-center overflow-hidden max-h-full text-xs lg:text-base antialiased font-extralight text-justify col-span-1 md:col-span-2 ${ isExpanded ? 'row-start-2' : 'row-start-7' } row-end-13 md:row-start-2 md:row-end-13 md:px-5 relative mt-2 md:mt-0`}>
+        { explanation }
+        <div className={`w-full h-1/3 absolute bottom-0 ${ isExpanded === false ? 'bg-opacity-90 bg-slate-200' : '' } flex sm:hidden justify-center items-end`}>
+          <button onClick={() => setExpanded(prev => !prev)} className='w-fit h-fit mb-2 border rounded-md flex bg-slate-950 text-slate-50 antialiased text-sm font-thin shrink-0 px-3 py-1'>{ isExpanded ? 'hide' : 'expand' }</button>
+        </div>
+      </small>
     </article>
   )
 };
