b48f88bd7f8be88f3dfd28b1b6c3d5f142c51af2
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-07 02:55:23 +0300

    fixed loading state for expanded card

--- src/components/CardExpandedLoading.tsx
+++ src/components/CardExpandedLoading.tsx
@@ -1,13 +1,9 @@
 export const CardExpandedLoading = () => {
   return (
-    <article className='w-2/4 h-full border rounded-md border-slate-900 shadow-lg bg-white flex flex-col gap-y-3 p-5 animate-pulse'>
-        <div className='w-full aspect-square rounded-md bg-slate-200'></div>
-            <h2 className='w-full bg-slate-200 h-7 rounded-md'></h2>
-            <p className='w-full bg-slate-200 h-20 rounded-md'></p>
-        <div className='w-full flex gap-x-3 h-7'>
-            <p className='w-7 h-full bg-slate-200 rounded-full'></p>
-            <p className='w-full h-full bg-slate-200 rounded-md'></p>
-        </div>
+    <article className='w-full h-full border-2 rounded-md border-slate-900 shadow-lg bg-slate-100 p-3 md:p-5 grid grid-cols-1 md:grid-cols-2 grid-rows-12 relative animate-pulse gap-3'>
+      <h2 className='bg-slate-200 w-full h-full text-center rounded-md text-md antialiased font-extralight shrink-0 col-span-1 md:col-span-2 row-start-1 row-end-2'></h2>
+      <div className={`border block rounded-md border-slate-200 bg-slate-200 col-span-1 row-start-2 row-end-7 md:row-start-2 md:row-end-13 relative`}></div>
+      <small className={`bg-slate-200 border rounded-md w-auto h-auto md:flex md:items-center max-h-full text-xs lg:text-base antialiased font-extralight text-justify col-span-1 md:col-span-2 row-start-7 overflow-hidden row-end-13 md:row-start-2 md:row-end-13 md:px-5 relative`}></small>
     </article>
   )
 };
--- src/features/ScrollTop.tsx
+++ src/features/ScrollTop.tsx
@@ -6,8 +6,8 @@ export const ScrollTop : React.FC<scrollButtonProps> = ({ container, scrolled })
   return (
     <>
       { scrolled && 
-        <button onClick={() => handleGoTop(container)} className="fixed flex justify-center items-center bottom-10 right-20 w-16 h-16 bg-slate-100 border border-slate-900 rounded-full hover:opacity-50 transition-all ease-in-out">
-          <img className='' src={ ArrowUp } />
+        <button onClick={() => handleGoTop(container)} className="fixed flex justify-center items-center bottom-3 lg:bottom-10 right-3 md:right-5 lg:right-20 w-16 h-16 bg-slate-900 lg:bg-slate-100 border border-slate-900 rounded-full hover:opacity-50 transition-all ease-in-out">
+          <img className='bg-white rounded-md lg:bg-transparent' src={ ArrowUp } />
         </button> 
       }
     </>
