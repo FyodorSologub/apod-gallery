bfc246f0c14141144744d2c740fd6f0f74b32904
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-07 03:01:11 +0300

    fixed error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -8,8 +8,8 @@ export const Error = () => {
     <div className='h-screen overflow-hidden pt-20'>
       <Header scrolled={0} hasScrollIndicator={ false } />
       <main className='h-full p-5 flex justify-center bg-slate-10'>
-        <section className='h-full snap-y snap-mandatory max-h-full w-4/5 max-w-[1000px] flex justify-center items-center gap-5 py-3 lg:py-5'>
-          <h2 className='text-4xl antialiased font-thin'>Something went wrong!</h2>
+        <section className='h-full max-h-full w-full md:w-4/5 md:max-w-[1000px] flex justify-center items-center gap-5 py-3 lg:py-5'>
+          <h2 className='text-xl lg:text-4xl antialiased font-thin'>Something went wrong!</h2>
           { isRouteErrorResponse(error) && ( error.data?.message && <p>{error.data.message}</p> ) }
         </section>
       </main>
