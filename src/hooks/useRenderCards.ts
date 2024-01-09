184a1c1f0325f2f1d433166ae7388b2a7c155c96
Fyodor Sologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-09 16:13:47 +0300

    started implementing filters

--- src/hooks/useRenderCards.ts
+++ src/hooks/useRenderCards.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSearchParams } from 'react-router-dom';
 import { RawApiData } from "../types";
 import { getNDaysBack } from "../utils";
 import { getApodData } from "../api";
@@ -6,13 +7,25 @@ import { setInSession, getFromSession, checkInSessionByDate } from "../utils";
 
 export const useRenderCards = ( isInViewport : boolean ) => {
     const [ data, setData ] = useState<RawApiData[]>([]);
+    const [ searchParams, setSearchParams ] = useSearchParams();
     const [ page, setPage ] = useState<number>(1);
     const [ isLoading, setLoading ] = useState<boolean>(false);
 
     useEffect(() => {
       setLoading(true);
-  
-      const endDate = new Date();
+      
+      const endDateParam = searchParams.get('endDate');
+      //const endDateParam = searchParams.get('endDate');
+
+      let endDate : Date;
+
+      if(endDateParam) {
+        if(new Date(endDateParam)) {
+          endDate = new Date(endDateParam);
+        } else { endDate = new Date(); }
+      } else { endDate = new Date(); }
+
+      const dateAsString = `${endDate.getUTCFullYear()}-${endDate.getUTCMonth() + 1 >= 10 ? endDate.getUTCMonth() + 1 : '0' + String(endDate.getUTCMonth() + 1)}-${endDate.getUTCDate() >= 10 ? endDate.getUTCDate() : '0' + String(endDate.getUTCDate())}`;
 
       const isInSession = checkInSessionByDate(endDate);
       const sessionData = getFromSession();
@@ -20,14 +33,14 @@ export const useRenderCards = ( isInViewport : boolean ) => {
       if(isInSession && sessionData !== null) { 
         setData(sessionData.slice(0,24)); 
         setInSession(sessionData.slice(0,24));
+        setSearchParams({ endDate : dateAsString });
         setLoading(false); 
       }
 
       if(!isInSession || sessionData === null) {
         const startDate = getNDaysBack(endDate, 23);
-  
         getApodData(startDate, endDate)
-        .then(json => { setData(json); setInSession(json); setLoading(false); });
+        .then(json => { setData(json); setInSession(json); setSearchParams({ endDate : dateAsString }); setLoading(false); });
       }
     }, []);
   
